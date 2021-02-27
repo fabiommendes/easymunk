2d9b974da262069caf8d332cf72111e9bf6aa212
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-02-27 14:44:08 -0300

    Add affine transform class

--- pymunk/transform.py
+++ pymunk/transform.py
@@ -1,6 +1,9 @@
 import math
 from typing import NamedTuple
 
+from .mat22 import Mat22
+from .vec2d import Vec2d, VecLike
+
 
 class Transform(NamedTuple):
     """Type used for 2x3 affine transforms.
@@ -106,7 +109,8 @@ class Transform(NamedTuple):
         Example to rotate by 1 rad:
 
         >>> Transform.rotation(1)
-        Transform(a=0.5403023058681398, b=0.8414709848078965, c=-0.8414709848078965, d=0.5403023058681398, tx=0, ty=0)
+        Transform(a=0.5403023058681398, b=0.8414709848078965, c=-0.8414709848078965,
+        d=0.5403023058681398, tx=0, ty=0)
 
         Returns a Transform with this matrix:
 
@@ -120,3 +124,95 @@ class Transform(NamedTuple):
         c = math.cos(t)
         s = math.sin(t)
         return Transform(a=c, b=s, c=-s, d=c)
+
+    @classmethod
+    def projection(cls, t, translation=(0, 0)):
+        """
+        Create a projection matrix.
+        """
+        return cls.affine(Mat22.projection(t), translation)
+
+    @classmethod
+    def affine(cls, matrix=None, translation=(0, 0)):
+        """
+        Create transform from linear transformation encoded in matrix and
+        translation.
+        """
+        a, b, c, d = Mat22.identity() if matrix is None else matrix
+        tx, ty = translation
+        return cls(a, b, c, d, tx, ty)
+
+    @classmethod
+    def similarity(
+        cls, *, scale=None, angle=None, angle_degrees=None, translation=(0, 0)
+    ):
+        """
+        Create affine transform from similarity transformations..
+        """
+        if angle is not None:
+            M = Mat22.rotation(angle)
+        elif angle_degrees is not None:
+            M = Mat22.rotation(angle_degrees)
+        else:
+            M = Mat22.identity()
+        vec = Vec2d(*translation)
+
+        if scale is not None:
+            vec *= scale
+            M = Mat22.scale(scale) * M
+
+        return cls.affine(M, vec)
+
+    def transform(self, vec: Vec2d):
+        """
+        Return transformed vector by affine transform.
+        """
+        x, y = vec
+        a, b, c, d, tx, ty = self
+        return Vec2d(a * x + b * y + tx, c * x + d * y + ty)
+
+    @property
+    def matrix(self):
+        """
+        Linear component of the transform.
+        """
+        return Mat22(self.a, self.b, self.c, self.d)
+
+    @property
+    def vector(self):
+        """
+        Translation vector.
+        """
+        return Vec2d(self.tx, self.ty)
+
+    def __mul__(self, other):
+        if isinstance(other, Mat22):
+            return Transform.affine(other * self.matrix, other.T * self.vector)
+        elif isinstance(other, Transform):
+            M = self.matrix
+            return Transform.affine(M * other.matrix, self.vector + M * other.vector)
+        elif isinstance(other, (tuple, Vec2d)):
+            return self.transform_vector(other)
+        return NotImplemented
+
+    def __rmul__(self, other):
+        if isinstance(other, Mat22):
+            return Transform.affine(other * self.matrix, other * self.vector)
+        return NotImplemented
+
+    def __add__(self, other):
+        if isinstance(other, (Vec2d, tuple)):
+            M = self.matrix
+            return Transform.affine(M, self.vector + M * other)
+        return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, (Vec2d, tuple)):
+            return Transform.affine(self.matrix, self.vector + other)
+        return NotImplemented
+
+    def transform_vector(self, vec: VecLike):
+        """
+        Transform vector by affine transform.
+        """
+        return self.matrix.transform_vector(vec) + self.vector

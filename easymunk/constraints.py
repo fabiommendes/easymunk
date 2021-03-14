8a16c493556c75397395245cd81a738f40738dfe
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-03-14 02:17:16 -0300

    Simplified pickling of constraints

--- easymunk/constraints.py
+++ easymunk/constraints.py
@@ -44,11 +44,9 @@ http://www.youtube.com/watch?v=ZgJJZTS0aMM
 
 Example::
 
->>> import easymunk
->>> import easymunk.constraints
->>> s = easymunk.Space()
->>> a,b = easymunk.Body(10,10), easymunk.Body(10,10)
->>> c = easymunk.constraints.PivotJoint(a, b, (0,0))
+>>> s = mk.Space()
+>>> a, b = mk.Body(10,10), mk.Body(10,10)
+>>> c = mk.PivotJoint(a, b, (0,0))
 >>> s.add(c)
 
 """
@@ -69,16 +67,16 @@ __all__ = [
 ]
 
 import logging
-from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Callable, Optional, TypeVar
 
 from ._chipmunk_cffi import ffi, lib
-from ._mixins import PickleMixin, TypingAttrMixing
 from .util import void, inner_constraints, cffi_body, init_attributes, cp_property
 from .vec2d import Vec2d, VecLike, vec2d_from_cffi
 
 if TYPE_CHECKING:
     from .body import Body
     from .space import Space
+    import easymunk as mk
 
 SolveFunc = Callable[["Constraint", "Space"], None]
 
@@ -86,6 +84,7 @@ REST_ANGLE = "The relative angle in radians that the bodies want to have"
 REST_LENGTH = "The distance the spring wants to be."
 STIFFNESS = "The spring constant (Young's modulus)."
 DAMPING = "How soft to make the damping of the spring."
+T = TypeVar('T')
 
 
 def constraint_property(attr, doc=None, wrap=None):
@@ -103,23 +102,19 @@ def anchor_property(name) -> Any:
     )
 
 
-class Constraint(PickleMixin, TypingAttrMixing):
+class Constraint:
     """Base class of all constraints.
 
     You usually don't want to create instances of this class directly, but
     instead use one of the specific constraints such as the PinJoint.
     """
 
-    _pickle_attrs_init = [*PickleMixin._pickle_attrs_init, "a", "b"]
-    _pickle_attrs_general = [
-        *PickleMixin._pickle_attrs_general,
-        "max_force",
-        "error_bias",
-        "max_bias",
-        "collide_bodies",
-    ]
-    _pickle_attrs_skip = [*PickleMixin._pickle_attrs_skip, "pre_solve", "post_solve"]
-    _init_kwargs = set(_pickle_attrs_general)
+    _pickle_args = "a", "b"
+    _pickle_kwargs = "max_force", "error_bias", "max_bias", "collide_bodies"
+    _pickle_meta_hide = {'_a', '_b', '_cffi_ref', "_cffi_backend", "_nursery",
+                         "_cp_post_solve_func", "_cp_pre_solve_func", "_post_solve_func",
+                         "_pre_solve_func"}
+    _init_kwargs = set(_pickle_args)
     _pre_solve_func: Optional[Callable[["Constraint", "Space"], None]] = None
     _post_solve_func: Optional[Callable[["Constraint", "Space"], None]] = None
     _cp_pre_solve_func: Any = ffi.NULL
@@ -254,36 +249,36 @@ class Constraint(PickleMixin, TypingAttrMixing):
         inner_constraints(b).add(self)
         init_attributes(self, self._init_kwargs, kwargs)
 
-    def __getstate__(self) -> Dict[str, List[Tuple[str, Any]]]:
-        """Return the state of this object
+    def __getstate__(self):
+        args = [getattr(self, k) for k in self._pickle_args]
+        meta = dict(self.__dict__)
+        for k in self._pickle_meta_hide:
+            meta.pop(k, None)
+        for k in self._pickle_kwargs:
+            meta[k] = getattr(self, k)
+        if hasattr(self, '_pre_solve_func'):
+            meta['pre_solve'] = self.pre_solve
+        if hasattr(self, '_post_solve_func'):
+            meta['post_solve'] = self.post_solve
+        return args, meta
 
-        This method allows the usage of the :mod:`copy` and :mod:`pickle`
-        modules with this class.
-        """
-        d = super(Constraint, self).__getstate__()
-
-        d["special"].append(("_pre_solve_func", self._pre_solve_func))
-        d["special"].append(("_post_solve_func", self._post_solve_func))
-
-        return d
-
-    def __setstate__(self, state: Dict[str, List[Tuple[str, Any]]]) -> None:
-        """Unpack this object from a saved state.
-
-        This method allows the usage of the :mod:`copy` and :mod:`pickle`
-        modules with this class.
-        """
-        super(Constraint, self).__setstate__(state)
-        for k, v in state["special"]:
-            if k == "_pre_solve_func" and v is not None:
-                self.pre_solve = v
-            elif k == "_post_solve_func" and v is not None:
-                self.post_solve = v
+    def __setstate__(self, state):
+        args, meta = state
+        self.__init__(*args)
+        for k, v in meta.items():
+            setattr(self, k, v)
 
     def activate_bodies(self) -> None:
         """Activate the bodies this constraint is attached to"""
         self._a.activate()
         self._b.activate()
+
+    def copy(self: T) -> T:
+        """Create a deep copy of this object."""
+        state = self.__getstate__()
+        new = object.__new__(type(self))
+        new.__setstate__(state)
+        return new
 
 
 class PinJoint(Constraint):
@@ -292,8 +287,7 @@ class PinJoint(Constraint):
     Keeps the anchor points at a set distance from one another.
     """
 
-    _pickle_attrs_init = Constraint._pickle_attrs_init + ["anchor_a", "anchor_b"]
-
+    _pickle_args = [*Constraint._pickle_args, "anchor_a", "anchor_b"]
     anchor_a: Vec2d = anchor_property("PinJointA")
     anchor_b: Vec2d = anchor_property("PinJointB")
     distance: float
@@ -304,12 +298,12 @@ class PinJoint(Constraint):
     )
 
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        anchor_a: VecLike = (0, 0),
-        anchor_b: VecLike = (0, 0),
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            anchor_a: VecLike = (0, 0),
+            anchor_b: VecLike = (0, 0),
+            **kwargs,
     ):
         """a and b are the two bodies to connect, and anchor_a and anchor_b are
         the anchor points on those bodies.
@@ -329,8 +323,8 @@ class SlideJoint(Constraint):
     from getting to far apart, but will allow them to get closer together.
     """
 
-    _pickle_attrs_init = [
-        *Constraint._pickle_attrs_init,
+    _pickle_args = [
+        *Constraint._pickle_args,
         "anchor_a",
         "anchor_b",
         "min",
@@ -349,23 +343,23 @@ class SlideJoint(Constraint):
 
     # noinspection PyShadowingBuiltins
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        anchor_a: VecLike,
-        anchor_b: VecLike,
-        min: float,
-        max: float,
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            anchor_a: VecLike,
+            anchor_b: VecLike,
+            min: float,
+            max: float,
+            **kwargs,
     ):
         """a and b are the two bodies to connect, anchor_a and anchor_b are the
         anchor points on those bodies, and min and max define the allowed
         distances of the anchor points.
         """
-        _constraint = lib.cpSlideJointNew(
-            cffi_body(a), cffi_body(b), anchor_a, anchor_b, min, max
-        )
-        super().__init__(a, b, _constraint, **kwargs)
+        ref_a = cffi_body(a)
+        ref_b = cffi_body(b)
+        ptr = lib.cpSlideJointNew(ref_a, ref_b, anchor_a, anchor_b, min, max)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class PivotJoint(Constraint):
@@ -374,16 +368,16 @@ class PivotJoint(Constraint):
     Its like a swivel.
     """
 
-    _pickle_attrs_init = [*Constraint._pickle_attrs_init, "anchor_a", "anchor_b"]
+    _pickle_args = [*Constraint._pickle_args, "anchor_a", "anchor_b"]
     anchor_a: Vec2d = anchor_property("PivotJointA")
     anchor_b: Vec2d = anchor_property("PivotJointB")
 
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        *args: Union[VecLike, Tuple[VecLike, VecLike]],
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            *args: VecLike,
+            **kwargs,
     ):
         """a and b are the two bodies to connect, and pivot is the point in
         world coordinates of the pivot.
@@ -397,21 +391,21 @@ class PivotJoint(Constraint):
         That is, either create the joint with PivotJoint(a, b, pivot) or
         PivotJoint(a, b, anchor_a, anchor_b).
 
-        :param Body a: The first of the two bodies
-        :param Body b: The second of the two bodies
-        :param args: Either one pivot point, or two anchor points
-        :type args: (float,float) or (float,float) (float,float)
+        Args:
+            a: The first of the two bodies
+            b: The second of the two bodies
+            args: Either one pivot point, or two anchor points
         """
+        ref_a = cffi_body(a)
+        ref_b = cffi_body(b)
         if len(args) == 1:
-            _constraint = lib.cpPivotJointNew(cffi_body(a), cffi_body(b), args[0])
+            ptr = lib.cpPivotJointNew(ref_a, ref_b, args[0])
         elif len(args) == 2:
-            _constraint = lib.cpPivotJointNew2(
-                cffi_body(a), cffi_body(b), args[0], args[1]
-            )
+            ptr = lib.cpPivotJointNew2(ref_a, ref_b, args[0], args[1])
         else:
             msg = "You must specify either one pivot point" " or two anchor points"
             raise TypeError(msg)
-        super().__init__(a, b, _constraint, **kwargs)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class GrooveJoint(Constraint):
@@ -421,8 +415,8 @@ class GrooveJoint(Constraint):
     being fixed.
     """
 
-    _pickle_attrs_init = [
-        *Constraint._pickle_attrs_init,
+    _pickle_args = [
+        *Constraint._pickle_args,
         "groove_a",
         "groove_b",
         "anchor_b",
@@ -444,13 +438,13 @@ class GrooveJoint(Constraint):
     )
 
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        groove_a: VecLike,
-        groove_b: VecLike,
-        anchor_b: VecLike,
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            groove_a: VecLike,
+            groove_b: VecLike,
+            anchor_b: VecLike,
+            **kwargs,
     ):
         """The groove goes from groove_a to groove_b on body a, and the pivot
         is attached to anchor_b on body b.
@@ -469,8 +463,8 @@ class DampedSpring(Constraint):
     The spring allows you to define the rest length, stiffness and damping.
     """
 
-    _pickle_attrs_init = [
-        *Constraint._pickle_attrs_init,
+    _pickle_args = [
+        *Constraint._pickle_args,
         "anchor_a",
         "anchor_b",
         "rest_length",
@@ -484,29 +478,28 @@ class DampedSpring(Constraint):
     damping: float = cp_property("DampedSpring", "Damping", doc=DAMPING)
 
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        anchor_a: VecLike,
-        anchor_b: VecLike,
-        rest_length: float,
-        stiffness: float,
-        damping: float,
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            anchor_a: VecLike,
+            anchor_b: VecLike,
+            rest_length: float,
+            stiffness: float,
+            damping: float,
+            **kwargs,
     ):
         """Defined much like a slide joint.
 
-        :param Body a: Body a
-        :param Body b: Body b
-        :param anchor_a: Anchor point a, relative to body a
-        :type anchor_a: `(float,float)`
-        :param anchor_b: Anchor point b, relative to body b
-        :type anchor_b: `(float,float)`
-        :param float rest_length: The distance the spring wants to be.
-        :param float stiffness: The spring constant (Young's modulus).
-        :param float damping: How soft to make the damping of the spring.
+        Args:
+            a: Body a
+            b: Body b
+            anchor_a: Anchor point a, relative to body a
+            anchor_b: Anchor point b, relative to body b
+            rest_length: The distance the spring wants to be.
+            stiffness: The spring constant (Young's modulus).
+            damping: How soft to make the damping of the spring.
         """
-        _constraint = lib.cpDampedSpringNew(
+        ptr = lib.cpDampedSpringNew(
             cffi_body(a),
             cffi_body(b),
             anchor_a,
@@ -515,14 +508,14 @@ class DampedSpring(Constraint):
             stiffness,
             damping,
         )
-        super().__init__(a, b, _constraint, **kwargs)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class DampedRotarySpring(Constraint):
     """DampedRotarySpring works like the DammpedSpring but in a angular fashion."""
 
-    _pickle_attrs_init = [
-        *Constraint._pickle_attrs_init,
+    _pickle_args = [
+        *Constraint._pickle_args,
         "rest_angle",
         "stiffness",
         "damping",
@@ -532,33 +525,33 @@ class DampedRotarySpring(Constraint):
     damping: float = cp_property("DampedRotarySpring", "Damping", doc=DAMPING)
 
     def __init__(
-        self,
-        a: "Body",
-        b: "Body",
-        rest_angle: float,
-        stiffness: float,
-        damping: float,
-        **kwargs,
+            self,
+            a: "Body",
+            b: "Body",
+            rest_angle: float,
+            stiffness: float,
+            damping: float,
+            **kwargs,
     ):
         """Like a damped spring, but works in an angular fashion.
 
-        :param Body a: Body a
-        :param Body b: Body b
-        :param float rest_angle: The relative angle in radians that the bodies
-            want to have
-        :param float stiffness: The spring constant (Young's modulus).
-        :param float damping: How soft to make the damping of the spring.
+        Args:
+            a: Body a
+            b: Body b
+            rest_angle: The relative angle in radians that the bodies want to have
+            stiffness: The spring constant (Young's modulus).
+            damping: How soft to make the damping of the spring.
         """
-        _constraint = lib.cpDampedRotarySpringNew(
-            cffi_body(a), cffi_body(b), rest_angle, stiffness, damping
-        )
-        super().__init__(a, b, _constraint, **kwargs)
+        ref_a = cffi_body(a)
+        ref_b = cffi_body(b)
+        ptr = lib.cpDampedRotarySpringNew(ref_a, ref_b, rest_angle, stiffness, damping)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class RotaryLimitJoint(Constraint):
     """RotaryLimitJoint constrains the relative rotations of two bodies."""
 
-    _pickle_attrs_init = [*Constraint._pickle_attrs_init, "min", "max"]
+    _pickle_args = [*Constraint._pickle_args, "min", "max"]
     min: float = cp_property("RotaryLimitJoint", "Min")
     max: float = cp_property("RotaryLimitJoint", "Max")
 
@@ -570,14 +563,14 @@ class RotaryLimitJoint(Constraint):
         that it's possible to for the range to be greater than a full
         revolution.
         """
-        _constraint = lib.cpRotaryLimitJointNew(cffi_body(a), cffi_body(b), min, max)
-        super().__init__(a, b, _constraint, **kwargs)
+        ptr = lib.cpRotaryLimitJointNew(cffi_body(a), cffi_body(b), min, max)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class RatchetJoint(Constraint):
     """RatchetJoint is a rotary ratchet, it works like a socket wrench."""
 
-    _pickle_attrs_init = [*Constraint._pickle_attrs_init, "phase", "ratchet"]
+    _pickle_args = [*Constraint._pickle_args, "phase", "ratchet"]
 
     def __init__(self, a: "Body", b: "Body", phase: float, ratchet: float, **kwargs):
         """Works like a socket wrench.
@@ -585,8 +578,8 @@ class RatchetJoint(Constraint):
         ratchet is the distance between "clicks", phase is the initial offset
         to use when deciding where the ratchet angles are.
         """
-        _constraint = lib.cpRatchetJointNew(cffi_body(a), cffi_body(b), phase, ratchet)
-        super().__init__(a, b, _constraint, **kwargs)
+        ptr = lib.cpRatchetJointNew(cffi_body(a), cffi_body(b), phase, ratchet)
+        super().__init__(a, b, ptr, **kwargs)
 
     angle: float = cp_property("RatchetJoint", "Angle")
     phase: float = cp_property("RatchetJoint", "Phase")
@@ -596,7 +589,7 @@ class RatchetJoint(Constraint):
 class GearJoint(Constraint):
     """GearJoint keeps the angular velocity ratio of a pair of bodies constant."""
 
-    _pickle_attrs_init = [*Constraint._pickle_attrs_init, "phase", "ratio"]
+    _pickle_args = [*Constraint._pickle_args, "phase", "ratio"]
     phase: float = cp_property("GearJoint", "Phase")
     ratio: float = cp_property("GearJoint", "Ratio")
 
@@ -607,14 +600,14 @@ class GearJoint(Constraint):
         possible to set the ratio in relation to a third body's angular
         velocity. phase is the initial angular offset of the two bodies.
         """
-        _constraint = lib.cpGearJointNew(cffi_body(a), cffi_body(b), phase, ratio)
-        super().__init__(a, b, _constraint, **kwargs)
+        ptr = lib.cpGearJointNew(cffi_body(a), cffi_body(b), phase, ratio)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 class SimpleMotor(Constraint):
     """SimpleMotor keeps the relative angular velocity constant."""
 
-    _pickle_attrs_init = [*Constraint._pickle_attrs_init, "rate"]
+    _pickle_args = [*Constraint._pickle_args, "rate"]
     rate: float
     rate = cp_property(  # type: ignore
         "SimpleMotor", "Rate", "Desired relative angular velocity"
@@ -627,8 +620,8 @@ class SimpleMotor(Constraint):
         to set an force (torque) maximum for motors as otherwise they will be
         able to apply a nearly infinite torque to keep the bodies moving.
         """
-        _constraint = lib.cpSimpleMotorNew(cffi_body(a), cffi_body(b), rate)
-        super().__init__(a, b, _constraint, **kwargs)
+        ptr = lib.cpSimpleMotorNew(cffi_body(a), cffi_body(b), rate)
+        super().__init__(a, b, ptr, **kwargs)
 
 
 def cffi_free_constraint(cp_constraint) -> None:
